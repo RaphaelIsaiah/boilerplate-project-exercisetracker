1249d472ff751ff75359ae6a314f6c769ec055d1
RaphaelIsaiah <rafvie12@gmail.com>
2025-04-28 17:04:22 +0100

    feat: integrate MongoDB connection and JSON body parsing in index.js

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+// Parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect DB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected!"))
+  .catch((err) => console.error("Connection error:", err));
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
